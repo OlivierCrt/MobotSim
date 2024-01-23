341246c67913364398a310012f95a2810038fccc
Solal <113097238+solaldl@users.noreply.github.com>
2024-01-23 18:59:20 +0100

    Update solal.h

--- solal.h
+++ solal.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void actionsPy(char nomfichier[], char *mat[5][4], int mat_compt, int *coin_HD, int rayon_bleu, int rayon_jaune,int rayon_orange,int *milieu_bleu,int *milieu_jaune, int *milieu_orange);
 void envPy(char nomfichier[],int *coin_HD, int rayon_bleu, int rayon_jaune,int rayon_orange,int *milieu_bleu,int *milieu_jaune,int *milieu_orange);
 
 #endif 
