1b9d293fde7b877b072a3fa4b697cf3d03eab0a5
Solal <113097238+solaldl@users.noreply.github.com>
2024-01-25 18:55:20 +0100

    Update solal.h

--- solal.h
+++ solal.h
@@ -4,7 +4,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void actionsPy(char nomfichier[], char *mat[5][4], int mat_compt, int *coin_HD, int rayon_bleu, int rayon_jaune,int rayon_orange,int *milieu_bleu,int *milieu_jaune, int *milieu_orange);
-void envPy(char nomfichier[],int *coin_HD, int rayon_bleu, int rayon_jaune,int rayon_orange,int *milieu_bleu,int *milieu_jaune,int *milieu_orange);
+void changementDeBase(int* coin_HD, int* milieu_bleu, int* milieu_jaune, int* milieu_orange);
+void modeliserEnvironnement(char nomfichier[],int *coin_HD, int rayon_bleu, int rayon_jaune,int rayon_orange,int *milieu_bleu,int *milieu_jaune,int *milieu_orange);
+void modeliserActions(char nomfichier[], char *mat[5][4], int mat_compt, int *coin_HD, int rayon_bleu, int rayon_jaune,int rayon_orange,int *milieu_bleu,int *milieu_jaune, int *milieu_orange);
 
 #endif 
